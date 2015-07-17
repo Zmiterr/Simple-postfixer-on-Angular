a8ddf589b6064133d5f6e58e49a837d8ef056a71
zmiterr <ozmiter@gmail.com>
2015-07-17 11:29:40 +0300

    Fix meta data

--- js/controllers.js
+++ js/controllers.js
@@ -6,14 +6,26 @@
 /* Controllers */
 var myApp = angular.module("myApp", []);
 myApp.controller("postfixerCtrl", function($scope){
+
+
     $scope.postfix = function(count) {
-        if($scope.count>=11&&$scope.count<=14){return countStr=""}
-        var countStr =  $scope.count.toString();
-        countStr =  countStr[countStr.length-1];
-        if(countStr==1){countStr="a"}
-        else if(countStr==2||countStr==3||countStr==4){countStr="û"}
-        else {countStr=""};
+        if ($scope.count >= 11 && $scope.count <= 14) {
+            return countStr = ""
+        }
+        var countStr = $scope.count.toString();
+        countStr = countStr[countStr.length - 1];
+        if (countStr == 1) {
+            countStr = "a"
+        }
+        else if (countStr == 2 || countStr == 3 || countStr == 4) {
+            countStr = "û"
+        }
+        else {
+            countStr = ""
+        }
+        ;
         return countStr;
+
     };
     $scope.$watch('text', function(text) {
 
